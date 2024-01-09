f23d37ec69da450752c7ec755e6c580fe5f0dda5
EdoBergamo <edo.bergamo03@icloud.com>
2024-01-09 07:45:01 -0800

    fix: check if link is active

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,50 +7,62 @@ export default {
         {
           nome: 'Characters',
           url: '#',
+          active: false
         },
         {
           nome: 'Comics',
           url: '#',
+          active: true
         },
         {
           nome: 'Movies',
           url: '#',
+          active: false
         },
         {
           nome: 'Tv',
           url: '#',
+          active: false
         },
         {
           nome: 'Games',
           url: '#',
+          active: false
         },
         {
           nome: 'Collectibles',
           url: '#',
+          active: false
         },
         {
           nome: 'Videos',
           url: '#',
+          active: false
         },
         {
           nome: 'Fans',
           url: '#',
+          active: false
         },
         {
           nome: 'News',
           url: '#',
+          active: false
         },
         {
           nome: 'Shop',
           url: '#',
+          active: false
         },
         {
           nome: 'Characters',
           url: '#',
+          active: false
         },
         {
           nome: 'Characters',
           url: '#',
+          active: false
         },
       ]
     }
@@ -68,7 +80,7 @@ export default {
 
       <ul class="list-links">
         <li class="link" v-for="li in menu">
-          <a :href="li.url" target="_blank">
+          <a :href="li.url" :class="li.active ? 'active' : ''" target="_blank">
             {{ li.nome }}
           </a>
         </li>
@@ -100,24 +112,29 @@ export default {
 }
 
 .list-links {
-    display: flex;
-    gap: 35px;
+  display: flex;
+  gap: 35px;
 
-    .link {
-      list-style: none;
-      line-height: 120px;
-      border-bottom: 2px solid transparent;
+  .link {
+    list-style: none;
+    line-height: 120px;
+    border-bottom: 2px solid transparent;
 
-      &:hover {
+    &:hover {
+      color: $bg-primary;
+      border-bottom: 2px solid $bg-primary;
+    }
+
+    a {
+      all: unset;
+      font-weight: 600;
+      text-decoration: none;
+
+      &.active {
         color: $bg-primary;
         border-bottom: 2px solid $bg-primary;
       }
-
-      a {
-        all: unset;
-        font-weight: 600;
-        text-decoration: none;
-      }
     }
   }
+}
 </style>
